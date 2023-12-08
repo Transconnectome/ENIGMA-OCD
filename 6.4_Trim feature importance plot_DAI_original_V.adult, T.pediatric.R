567d641cd7183d231b93488fe877b871a395edd5
Gakyung kim <64996099+GakyungKim@users.noreply.github.com>
2023-12-08 11:12:58 +0900

    6.4_Trim feature importance plot_DAI_original_V.adult, T.pediatric.R

--- 6.4_Trim feature importance plot_DAI_original_V.adult, T.pediatric.R
+++ 6.4_Trim feature importance plot_DAI_original_V.adult, T.pediatric.R
@@ -3,16 +3,15 @@
 rm(list = ls())
 
 #### Load codebook #####
-setwd("C:/Users/±èº¸°â/Desktop/Connectome/study-enigma ocd/ENIGMA-OCD/0.Data/Codebook")
+setwd("../ENIGMA-OCD/0.Data/Codebook")
 df_cb <- read_csv('Codebook_v.Base.csv') # codebook:  254 x 5
 df_cb_ <- read_csv('Codebook_v._.csv') # codebook:  254 x 5
-
 
 df_cb
 
 
 #### Validation on adult, test on pediatric #####
-setwd("C:/Users/±èº¸°â/Desktop/Connectome/study-enigma ocd/ENIGMA-OCD/3.Documentations/Tables")
+setwd("../ENIGMA-OCD/3.Documentations/Tables")
 
 df_fi <- read_csv('Feature importance_DAI_V.adult.T.pediatric.csv')
 # Trim
@@ -27,5 +26,5 @@ df_fi %>%
   relocate(Rank) -> df_fi_trimmed #  6 x 4
 
 # save output
-setwd("C:/Users/±èº¸°â/Desktop/Connectome/study-enigma ocd/ENIGMA-OCD/3.Documentations/Tables")
+setwd("../ENIGMA-OCD/3.Documentations/Tables")
 write.csv(df_fi_trimmed, 'Feature importance_DAI_V.adult.T.pediatric_trimmed.csv' , row.names = F)
